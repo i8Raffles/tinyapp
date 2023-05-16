a7204d659dbcca6da324520d1d897c2cf6c1414c
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-15 22:04:27 -0400

    updated server to respond to redirects

--- express_server.js
+++ express_server.js
@@ -48,9 +48,17 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  const templateVars = { id, longURL };
+  res.render("urls_show", templateVars);
+  console.log(req.body); // Log the POST request body to the console  
 });
 
 app.get("/urls.json", (req, res) => {
