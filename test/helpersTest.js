25fd5b66fec029e2a177a8f1cd78de3813232b5a
Md Muhtasim Yasser <muhtasimyasser@gmail.com>
2023-05-19 15:35:09 -0400

    added mocha chai testing for another helper function

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -23,5 +23,42 @@ describe('getUserByEmail', function() {
   it('should return undefined when the email does not exist in users database', function() {
     const user = getUserByEmail("somemail@example.com", testUsers)
     assert.equal(user, undefined);
+  });
+});
+
+const Urls = {
+  'erqr': {
+    longURL: 'http://www.reddit.com',
+    userID: 'morgan'
+  },
+  'jyth': {
+    longURL: 'http://www.google.com',
+    userID: 'henry'
+  },
+  'eweq': {
+    longURL: 'http://www.leagueoflegends.com',
+    userID: 'morgan'
+  }
+};
+describe('#urlsForUser', () => {
+  it('should return the corresponding urls for a valid user', () => {
+    const userUrls = urlsForUser('morgan', Urls);
+    const expectedResult = {
+      'erqr': {
+        longURL: 'http://www.reddit.com',
+        userID: 'morgan'
+      },
+      'eweq': {
+        longURL: 'http://www.leagueoflegends.com',
+        userID: 'morgan'
+      }
+    };
+
+    assert.deepEqual(userUrls, expectedResult);
+  });
+
+  it('should return an empty object for a non-existent user', () => {
+    const userUrls = urlsForUser('jimmy', Urls);
+    assert.deepEqual(userUrls, {});
   });
 });
